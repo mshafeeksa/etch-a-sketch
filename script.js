8ef6ca8d6edcfae98c703b97b7fe5853b4332af4
Mohammed Shafeek <mail2shafeek.sa@gmai.com>
2023-11-02 19:33:10 +0400

    Add RandomRGB and Progressive darkening

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ let randomRgbColoring = false;
 color = "rgb(0,0,0)"
 drawGrid(boxSize);
 const blankButton = document.querySelector("#blank");
-const changeSizeButton = document.querySelector("#change-size");
 const progressiveButton = document.querySelector("#gradual");
+const randomRgbButton = document.querySelector("#random-rgb");
+const changeSizeButton = document.querySelector("#change-size");
 const sizeDisplay = document.querySelector("#display-size");
 sizeDisplay.textContent = boxSize + "X" + boxSize;
 
 
 
-function drawGrid(boxSize)
+function drawGrid(boxSize, boxOpacity)
 {   
     const grid = document.querySelector(".grid");
     grid.style.display = "flex";
@@ -29,22 +30,29 @@ function drawGrid(boxSize)
             box = document.createElement("div");
             box.style.minWidth = boxSizePercent+"%";
             box.style.height = boxSizePercent+"%";
-            box.style.border = "1px solid black";
+            // box.style.border = "1px solid black";
+            box.style.opacity = boxOpacity;
             box.classList.add("grid-box");
             grid.appendChild(box);
             // box.addEventListener("mouseover",(event)=>{
             //     changeColor(event.target, progressiveColoring, randomRgbColoring? getRandomColor():color);
             box.addEventListener("mouseover",(event)=>{
-                changeColor(event.target, true, randomRgbColoring? getRandomColor():color);
+                changeColor(event.target, progressiveColoring, randomRgbColoring? getRandomColor():color);
             })
         }
     }
 }
 
 
-blankButton.addEventListener("click", ()=> {
-    deleteGrid(boxSize)
-    drawGrid(boxSize)});
+blankButton.addEventListener("click", ()=> resetGrid());
+    
+function resetGrid(progressiveColoring){
+    deleteGrid(boxSize);
+    if (progressiveColoring === true)
+        drawGrid(boxSize,0);
+    else
+        drawGrid(boxSize,1);
+}
 
 function deleteGrid(boxSize){
     let box;
@@ -69,12 +77,46 @@ function changeColor(target, progressiveColoring, color)
 {   
     const compStyle = window.getComputedStyle(target);
     const opacity = compStyle.getPropertyValue("opacity");
-    if(progressiveColoring === "true")
+    if(progressiveColoring === true)
     {
-
+        target.style.backgroundColor = color;
+        if (opacity < 1)
+            target.style.opacity = +opacity + 0.1;
     }
     else
     {
         target.style.backgroundColor = color;
+        target.style.opacity = 1;
     }
+}
+
+progressiveButton.addEventListener("click",()=>{
+    progressiveButton.classList.toggle("active-button");
+    progressiveColoring = !progressiveColoring;
+    if (progressiveColoring)
+        {    
+            progressiveButton.textContent = "Progressive Darkening (ON)";
+            resetGrid(progressiveColoring);
+
+        }
+    else
+        progressiveButton.textContent = "Progressive Darkening (OFF)";
+});
+
+randomRgbButton.addEventListener("click",()=>{
+    randomRgbButton.classList.toggle("active-button");
+    randomRgbColoring = !randomRgbColoring;
+    if (randomRgbColoring)    
+        randomRgbButton.textContent = "Random RGB Effect (ON)";
+    else
+        randomRgbButton.textContent = "Random RGB Effect (OFF)";
+});
+
+function getRandomColor()
+{
+    let red,green,blue;
+    red = Math.floor(Math.random() * 256);
+    green = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
+    return "rgb("+red+","+green+","+blue+")";
 }
